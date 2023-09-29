63f85ab6728024b8977f80bd29f8890b4e1af15e
Brezker <brezker10@gmail.com>
2023-09-28 19:24:23 -0600

    Primeras interfaces listas

---
+++ app/src/main/java/com/example/public_transport_android/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.public_transport_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        // En el método onCreate de tu actividad
+        window.decorView.systemUiVisibility = (
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        or View.SYSTEM_UI_FLAG_FULLSCREEN
+                        or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+
+                )
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
